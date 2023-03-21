82b9e1a5f345b53af0d0919bf179022852572b6c
Oleksii <oleksii.y@peeklogic.com>
2023-03-21 15:52:59 +0200

    ok

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const pali = document.getElementById('pali');
 const ui = document.getElementById('ui');
 const tdd = document.getElementById('tdd')
 const p = document.getElementById('p');
+let result;
 document.getElementById('btn').addEventListener('click', () => {
-    document.getElementById('result').innerText += Number(adf.value) * 0.15 + 
+    result = Number(adf.value) * 0.15 + 
     Number(pali.value) * 0.27 +
     Number(ui.value) * 0.2 + Number(tdd.value) * 0.2 + Number(p.value) * 0.18;
+    document.getElementById('result').innerText += " " + result;
 });
